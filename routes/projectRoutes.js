d977ef494db286b18ecb32c797efc1c84bc8dd92
leianne louis <leilouis112@gmail.com>
2019-02-15 11:37:13 -0500

    added post for projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,6 +2,21 @@ const express = require('express');
 const db = require('../data/helpers/projectModel');
 const router = express();
 
+router.post('/', async (req, res) => {
+    const project = req.body
+    console.log(project)
+    try {
+        const newProject = await db.insert(project)
+        if(newProject) {
+            res.status(201).json({ message: "your project was created" })
+        } else {
+            res.status(404).json({ message: "there is an issue with the info you sent" })
+        }
+    }
+    catch(error) {
+        res.status(500).json({ message: "nott working!!!!"})
+    }
+})
 router.get('/', async (req, res) => {
     try {
         const projects = await db.get()
@@ -16,4 +31,5 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('')
 module.exports = router;
