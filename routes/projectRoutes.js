ef57cc655fabaaf07c2e04609df81b6e66bd1b14
leianne louis <leilouis112@gmail.com>
2019-02-15 11:26:45 -0500

    read added for projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const db = require('../data/dbConfig');
+const db = require('../data/helpers/projectModel');
 const router = express();
 
-// router.get()
+router.get('/', async (req, res) => {
+    try {
+        const projects = await db.get()
+        if(projects) {
+            res.status(200).json(projects)
+        } else {
+            res.status(404).json({ message: "not working@"})
+        }
+    }
+    catch(error) {
+        res.status(500).json({ message: "nott working!!!!"})
+    }
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 const projectRoutes = require('./routes/projectRoutes');
 
+server.use(express.json())
 server.use('/api/projects', projectRoutes)
 
 module.exports = server;
